bb86de7380624dfc8c72ece30a9be352e497c33c
Natanael Arndt <arndtn@gmail.com>
2020-12-22 12:51:07 +0100

    Use updated version of function from https://github.com/aioobe/jekyll/blob/62a44661b7a5d65aa4571e111bca7f168c8a97a0/lib/jekyll/filters/url_filters.rb

--- _plugins/relativize_url.rb
+++ _plugins/relativize_url.rb
@@ -1,11 +1,22 @@
 require 'pathname'
 
+# Produces a path relative to the current page. For example, if the path
+# `/images/me.jpg` is supplied, then the result could be, `me.jpg`,
+# `images/me.jpg` or `../../images/me.jpg` depending on whether the current
+# page lives in `/images`, `/` or `/sub/dir/` respectively.
+#
+# input - a path relative to the project root
+#
+# Returns the supplied path relativized to the current page.
+
 module Jekyll
   module UrlRelativizer
-    def relativize_url(url)
-      pageUrl = @context.registers[:page]["url"]
-      pageDir = Pathname(pageUrl).parent
-      Pathname(url).relative_path_from(pageDir).to_s
+    def relativize_url(input)
+      return if input.nil?
+      input = ensure_leading_slash(input)
+      page_url = @context.registers[:page]["url"]
+      page_dir = Pathname(page_url).parent
+      Pathname(input).relative_path_from(page_dir).to_s
     end
   end
 end
